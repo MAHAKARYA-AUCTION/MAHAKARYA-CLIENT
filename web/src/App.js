90cb2c2379862db0f94f50e0243fbdeb1d0d00aa
Jonathan Sebastian <j.shn50@gmail.com>
2022-04-13 00:53:05 +0700

    login page

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
 import { Routes, Route } from "react-router-dom";
 import './App.css';
+import LoginPage from '../src/views/LoginPage';
+
+import LandingView from "./views/LandingView";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-[#ebd7bb] min-h-screen">
       <Routes>
         <Route path="/" element={<LandingView />} />
+        <Route
+          path='/login'
+          element={
+              <LoginPage />
+          }
+        />
       </Routes>
     </div>
   );
