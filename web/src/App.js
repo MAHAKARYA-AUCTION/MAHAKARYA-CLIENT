702a01f66ab41754f6639f7ae413b73765d6aac9
unknown <daharrisa@gmail.com>
2022-04-12 13:22:58 +0700

    done installing a-frame and creating route for virtual hall

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import { Routes, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
+import VirtualAuctionHall from "./views/VirtualAuctionHall";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<LandingView />} />
+        <Route path="/hall" element={<VirtualAuctionHall />} />
       </Routes>
     </div>
   );
