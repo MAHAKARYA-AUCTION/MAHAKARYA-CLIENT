50529f9b1f2e84daa2f1232ec3b8657519557948
fadhilM <fadhilm614@gmail.com>
2022-04-12 12:19:27 +0700

    add browser router and color pallete

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 
+import LandingView from "./views/LandingView";
+
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-[#ebd7bb]">
       <Routes>
         <Route path="/" element={<LandingView />} />
       </Routes>
